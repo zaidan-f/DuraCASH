77f00ee8a723c1d1a7fa17e754308c9c681c0646
zaidan-f <zaidan22fajar@gmail.com>
2024-02-29 09:25:29 +0700

    mengganti warna chart dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,14 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
             {
                 label: 'Stok Masuk',
                 data: [150, 250, 600, 200, 250, 700, 800, 900, 200, 300, 150, 200],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                backgroundColor: 'rgba(75, 192, 192, 0.5)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1
             },
             {
                 label: 'Stok Keluar',
                 data: [123, 163, 423, 132, 134, 312, 521, 731, 123, 114, 124, 163],
-                backgroundColor: 'rgba(196, 46, 29, 0.2)',
+                backgroundColor: 'rgba(196, 46, 29, 0.5)',
                 borderColor: 'rgba(196, 46, 29, 1)',
                 borderWidth: 1
             }
@@ -40,15 +40,15 @@ document.addEventListener('DOMContentLoaded', function () {
             {
                 label: 'Pemasukan',
                 data: [500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600],
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
+                backgroundColor: 'rgba(46, 245, 39, 0.5)',
+                borderColor: 'rgba(46, 245, 39, 1)',
                 borderWidth: 1
             },
             {
                 label: 'Pengeluaran',
                 data: [200, 150, 180, 250, 300, 280, 320, 350, 380, 400, 420, 450],
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'rgba(54, 162, 235, 1)',
+                backgroundColor: 'rgba(206, 0, 0, 0.5)',
+                borderColor: 'rgba(206, 0, 0, 1)',
                 borderWidth: 1
             }
         ]
